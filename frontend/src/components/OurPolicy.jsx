7c22979bfdd71137d8c04b4e6af702d5c1276a59
Ransford Oduro <100471914+ranscky@users.noreply.github.com>
2026-02-16 20:41:02 +0000

    added a newsletter and policy section

---
+++ frontend/src/components/OurPolicy.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const OurPolicy = () => {
+  return (
+    <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2  text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
+        <div>
+            <img src={assets.exchange_icon} alt="" className='w-12 m-auto mb-5' />
+            <p className='font-semibold'>Easy exchange policy</p>
+            <p className='text-gray-400'>We offer hassle free exchange of goods.</p>
+        </div>
+
+        <div>
+            <img src={assets.quality_icon} alt="" className='w-12 m-auto mb-5' />
+            <p className='font-semibold'>7 day return policy</p>
+            <p className='text-gray-400'>We provide seven days free return policy.</p>
+        </div>
+
+        <div>
+            <img src={assets.support_img} alt="" className='w-12 m-auto mb-5' />
+            <p className='font-semibold'>Best customer support</p>
+            <p className='text-gray-400'>We provide 24/7 customer support</p>
+        </div>
+    </div>
+  )
+}
+
+export default OurPolicy
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import Bestseller from '../components/Bestseller'
+import OurPolicy from '../components/OurPolicy'
+import Newsletter from '../components/Newsletter'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <LatestCollection />
       <Bestseller />
+      <OurPolicy />
+      <Newsletter />
     </div>
   )
 }
